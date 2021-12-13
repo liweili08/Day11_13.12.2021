37f5f0c7f06e6b7ef9914b1886b4cb86cd86b8e0
liwei <liweili@qq.com>
2021-12-13 19:53:24 +0100

    PathVariable & Unterschied zwischen RequestParam notiert

--- src/main/java/de/liwei/controller/StudentController.java
+++ src/main/java/de/liwei/controller/StudentController.java
@@ -2,10 +2,7 @@ package de.liwei.controller;
 
 
 import de.liwei.model.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("student")
@@ -17,13 +14,35 @@ public class StudentController {
     }
 
     @GetMapping(path = "{id}")
-    public String getStudent(@PathVariable int id) {
+    public String getStudentName(@PathVariable int id) {
         Student student = new Student("Daniel", 2021002, 19);
         if (id == student.getStudentId()){
             return student.getName();
         }
         return "nicht gefunden";
     }
+    //localhost:8080/student/2021002
+
+
+//    @GetMapping("search")
+//    //@GetMapping(value = "search")
+//    public String search(@RequestParam String name) {
+//        Student student = new Student("Daniel", 2021002, 19);
+//        if (name.equals(student.getName())){
+//            return student.toString();
+//        }
+//        return "nicht gefunden";
+//    }
+
+    @GetMapping(path = "search/{name}")
+    public String search(@PathVariable String name) {
+        Student student = new Student("Daniel", 2021002, 19);
+        if (name.equals(student.getName())){
+            return student.toString();
+        }
+        return "nicht gefunden";
+    }
+
 
 
 }
